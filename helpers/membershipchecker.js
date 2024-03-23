a7db74965df41e6148600c9d549b1d5a155a8490
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2024-03-23 17:28:39 +0530

    73

--- helpers/membershipchecker.js
+++ helpers/membershipchecker.js
@@ -11,172 +11,121 @@ exports.middlewareMembership = async (req, res, next) => {
 		const membershipid = user.memberships.membership
 		const membership = await Membership.findById(membershipid)
 		const membershipEndingDate = new Date(user.memberships.ending);
-		if (membership._id.toString() === "65671e5204b7d0d07ef0e796") {
 
-			const productlimit = user.limits.productlimit
-			const topiclimit = user.limits.topiclimit
-			const communitylimit = user.limits.communitylimit
-			const collectionlimit = user.limits.collectionlimit
+		if (currentDay < membershipEndingDate.getTime()) {
+			if (membership.title !== "Custom") {
+				const productlimit = membership.productlimit
+				const topiclimit = membership.topiclimit
+				const communitylimit = membership.communitylimit
+				const collectionlimit = membership.collectionlimit
 
+				let canCreateCollection;
+				let canCreateProduct;
+				let canCreateCommunity;
+				let cancreatetopic;
 
-			let canCreateCollection;
-			let canCreateProduct;
-			let canCreateCommunity;
-			let cancreatetopic;
+				if (user.collectionss.length < collectionlimit) {
+					canCreateCollection = true
 
-			if (user.collectionss.length < collectionlimit) {
-				canCreateCollection = true
-
-			} else {
-				canCreateCollection = false
-			}
-			const community = await Community.find({ creator: userId })
-
-			if (community.length < communitylimit) {
-				canCreateCommunity = true
-			} else {
-				canCreateCommunity = false
-			}
-			if (comId) {
-				const community = await Community.findById(comId)
-				if (community.topics.length < topiclimit) {
-					cancreatetopic = true
 				} else {
-					cancreatetopic = false
+					canCreateCollection = false
 				}
-			}
+				const community = await Community.find({ creator: userId })
 
-			if (colid) {
-				const collection = await Collection.findById(colid)
-				if (collection) {
-					if (collection.products.length < productlimit) {
-						canCreateProduct = true
+				if (community.length < communitylimit) {
+					canCreateCommunity = true
+				} else {
+					canCreateCommunity = false
+				}
+				if (comId) {
+					const community = await Community.findById(comId)
+					if (community.topics.length < topiclimit) {
+						cancreatetopic = true
 					} else {
-						canCreateProduct = false
+						cancreatetopic = false
 					}
 				}
+
+				if (colid) {
+					const collection = await Collection.findById(colid)
+					if (collection) {
+						if (collection.products.length < productlimit) {
+							canCreateProduct = true
+						} else {
+							canCreateProduct = false
+						}
+					}
+				}
+				req.memberships = true
+				req.canCreateCollection = canCreateCollection
+				req.canCreateCommunity = canCreateCommunity
+				req.canCreateProduct = canCreateProduct
+				req.cancreatetopic = cancreatetopic
+				next()
+			} else {
+				console.log("custom hua run")
+				const productlimit = user.limits.productlimit
+				const topiclimit = user.limits.topiclimit
+				const communitylimit = user.limits.communitylimit
+				const collectionlimit = user.limits.collectionlimit
+
+
+				let canCreateCollection;
+				let canCreateProduct;
+				let canCreateCommunity;
+				let cancreatetopic;
+
+				if (user.collectionss.length < collectionlimit) {
+					canCreateCollection = true
+
+				} else {
+					canCreateCollection = false
+				}
+				const community = await Community.find({ creator: userId })
+
+				if (community.length < communitylimit) {
+					canCreateCommunity = true
+				} else {
+					canCreateCommunity = false
+				}
+				if (comId) {
+					const community = await Community.findById(comId)
+					if (community.topics.length < topiclimit) {
+						cancreatetopic = true
+					} else {
+						cancreatetopic = false
+					}
+				}
+
+				if (colid) {
+					const collection = await Collection.findById(colid)
+					if (collection) {
+						if (collection.products.length < productlimit) {
+							canCreateProduct = true
+						} else {
+							canCreateProduct = false
+						}
+					}
+				}
+				req.memberships = true
+				req.canCreateCollection = canCreateCollection
+				req.canCreateCommunity = canCreateCommunity
+				req.canCreateProduct = canCreateProduct
+				req.cancreatetopic = cancreatetopic
+				next()
 			}
-			req.memberships = true
-			req.canCreateCollection = canCreateCollection
-			req.canCreateCommunity = canCreateCommunity
-			req.canCreateProduct = canCreateProduct
-			req.cancreatetopic = cancreatetopic
-			next()
+
 		} else {
-			if (currentDay < membershipEndingDate.getTime()) {
-				if (membership.title !== "Custom") {
-					const productlimit = membership.productlimit
-					const topiclimit = membership.topiclimit
-					const communitylimit = membership.communitylimit
-					const collectionlimit = membership.collectionlimit
-
-					let canCreateCollection;
-					let canCreateProduct;
-					let canCreateCommunity;
-					let cancreatetopic;
-
-					if (user.collectionss.length < collectionlimit) {
-						canCreateCollection = true
-
-					} else {
-						canCreateCollection = false
-					}
-					const community = await Community.find({ creator: userId })
-
-					if (community.length < communitylimit) {
-						canCreateCommunity = true
-					} else {
-						canCreateCommunity = false
-					}
-					if (comId) {
-						const community = await Community.findById(comId)
-						if (community.topics.length < topiclimit) {
-							cancreatetopic = true
-						} else {
-							cancreatetopic = false
-						}
-					}
-
-					if (colid) {
-						const collection = await Collection.findById(colid)
-						if (collection) {
-							if (collection.products.length < productlimit) {
-								canCreateProduct = true
-							} else {
-								canCreateProduct = false
-							}
-						}
-					}
-					req.memberships = true
-					req.canCreateCollection = canCreateCollection
-					req.canCreateCommunity = canCreateCommunity
-					req.canCreateProduct = canCreateProduct
-					req.cancreatetopic = cancreatetopic
-					next()
-				} else {
-					console.log("custom hua run")
-					const productlimit = user.limits.productlimit
-					const topiclimit = user.limits.topiclimit
-					const communitylimit = user.limits.communitylimit
-					const collectionlimit = user.limits.collectionlimit
-
-
-					let canCreateCollection;
-					let canCreateProduct;
-					let canCreateCommunity;
-					let cancreatetopic;
-
-					if (user.collectionss.length < collectionlimit) {
-						canCreateCollection = true
-
-					} else {
-						canCreateCollection = false
-					}
-					const community = await Community.find({ creator: userId })
-
-					if (community.length < communitylimit) {
-						canCreateCommunity = true
-					} else {
-						canCreateCommunity = false
-					}
-					if (comId) {
-						const community = await Community.findById(comId)
-						if (community.topics.length < topiclimit) {
-							cancreatetopic = true
-						} else {
-							cancreatetopic = false
-						}
-					}
-
-					if (colid) {
-						const collection = await Collection.findById(colid)
-						if (collection) {
-							if (collection.products.length < productlimit) {
-								canCreateProduct = true
-							} else {
-								canCreateProduct = false
-							}
-						}
-					}
-					req.memberships = true
-					req.canCreateCollection = canCreateCollection
-					req.canCreateCommunity = canCreateCommunity
-					req.canCreateProduct = canCreateProduct
-					req.cancreatetopic = cancreatetopic
-					next()
+			user.ismembershipactive = true,
+				user.memberships = {
+					membership: "65671e5204b7d0d07ef0e796",
+					ending: "infinite",
+					status: true
 				}
-
-			} else {
-				user.ismembershipactive = true,
-					user.memberships = {
-						membership: "65671e5204b7d0d07ef0e796",
-						ending: "infinite",
-						status: true
-					}
-				await user.save()
-				res.status(203).json({ success: false })
-			}
+			await user.save()
+			res.status(203).json({ success: false })
 		}
+
 	} catch (error) {
 		res.status(400).json({ success: false })
 		console.log(error)
